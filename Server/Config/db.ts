39f9236617411c2f624a5ed5cb41c075550fa8d0
Parulgautam4406 <124847879+Parulgautam4406@users.noreply.github.com>
2023-07-24 08:53:03 +0530

    added db.ts

--- Server/Config/db.ts
+++ Server/Config/db.ts
@@ -1,5 +1,5 @@
 let localURI = 'mongodb://127.0.0.1:27017/movies';
-let remoteURI = 'mongodb+srv://thomas:VENcSV4FZZzTOvMR@moviecluster.yf0nclt.mongodb.net/movies';
+let remoteURI = 'mongodb+srv://parulgautam4406:cN0AcWQk9MmWXn19@moviecluster.fjrkxch.mongodb.net/movies';
 let secret = 'SomeSecret';
 
 export default {
